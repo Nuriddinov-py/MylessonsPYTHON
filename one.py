c453c0d39fd520e88a147584d8e3eacf56ec62a6
Nuriddinov-py <khvichatrade@gmail.com>
2026-07-18 16:08:33 +0500

    nimadurda

--- one.py
+++ one.py
@@ -1,1 +1,4 @@
 print("Ertaga Farnsiya va Angliya o'rtasida futbol o'yini bo'ladi")
+print("Hello word")
+print("Nuriddinov Odiljon")
+print("This is my firs program")
---
+++ uuu.py
@@ -0,0 +1,6 @@
+print("Vazifa 1: Python dasturlash tilini o'rganish")
+
+def main(add1, add2):
+    return  add1 + add2
+    
+    
